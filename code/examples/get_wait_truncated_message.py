8ceb2f97db6a914c6a058c35852bee108c938eb9
ibmmqmet <marke_taylor@uk.ibm.com>
2026-05-11 09:22:00 +0100

    New sample: read truncated messages Updated sample: process Event messages

---
+++ code/examples/get_wait_truncated_message.py
@@ -0,0 +1,49 @@
+# More examples are at https://github.com/ibm-messaging/mq-dev-patterns
+# and in code/examples in the source distribution.
+
+"""
+This example shows reading a single message from a queue, waiting
+for up to 5 seconds before giving up. In this scenario, we allow
+the message to be truncated by giving a tiny buffer.
+"""
+
+import ibmmq as mq
+
+queue_manager = 'QM1'
+channel = 'DEV.APP.SVRCONN'
+host = '127.0.0.1'
+port = '1414'
+queue_name = 'DEV.QUEUE.1'
+conn_info = '%s(%s)' % (host, port)
+user = 'app'
+password = 'password'
+WAIT_INTERVAL = 5  # seconds
+
+# Message Descriptor
+md = mq.MD()
+
+# Get Message Options
+gmo = mq.GMO()
+gmo.Options = mq.CMQC.MQGMO_WAIT | mq.CMQC.MQGMO_FAIL_IF_QUIESCING
+gmo.Options |= mq.CMQC.MQGMO_ACCEPT_TRUNCATED_MSG
+gmo.WaitInterval = WAIT_INTERVAL * 1000
+
+qmgr = mq.connect(queue_manager, channel, conn_info, user, password)
+
+queue = mq.Queue(qmgr, queue_name)
+print("Waiting for message ...")
+try:
+  max_length = 2  # Something tiny to demonstrate that truncated messages can be handled
+  message = queue.get(max_length, md, gmo)
+except mq.MQMIError as e:
+  if e.comp == mq.CMQC.MQCC_WARNING and e.reason == mq.CMQC.MQRC_TRUNCATED_MSG_ACCEPTED:
+      print(e)
+      print(f'Original length: {e.original_length}')
+      message=e.message
+  else:
+      raise e
+
+print("Message: ", message)
+queue.close()
+
+qmgr.disconnect()
--- code/examples/hdr_browse.py
+++ code/examples/hdr_browse.py
@@ -4,11 +4,41 @@
 """
 This example browses a queue, formatting any known MQ header structures that it finds. It is
 an extension to the DLH example, in that it deals with a number of other headers
-including the XQH found on messages on transmission queues.
+including the XQH found on messages on transmission queues. It also decodes PCF event messages.
 """
+
+import sys
 
 import ibmmq as mq
 from ibmmq import CMQC
+
+def lookup(k):
+    '''Convert a PCF field to the corresponding string.
+    '''
+
+    s = mq.CMQSTRC.MQIA_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQIACF_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQIACH_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQIAMO_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQIAMO64_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQCA_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQCACF_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQCACH_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQCAMO_DICT.get(k)
+    if s is None:
+        s = mq.CMQSTRC.MQBACF_DICT.get(k)
+
+    if s is None:
+        s = f"<UNKNOWN> [{str(k)}]"
+    return s
 
 queue_manager = 'QM1'
 channel = 'DEV.ADMIN.SVRCONN'
@@ -23,8 +53,14 @@ qmgr = mq.connect(queue_manager, channel, conn_info, user, password)
 ok = True
 
 od = mq.OD()
-# This is an XMITQ associated with a STOPPED channel, because we want to look at XQH processing
-od.ObjectName = 'QM2.STOPPED'
+
+try:
+    # If the qname is given as the only parm on command line, accept that
+    od.ObjectName = sys.argv[1]
+except:
+    # This is an XMITQ associated with a STOPPED channel, because we want to look at XQH processing
+    od.ObjectName = 'QM2.STOPPED'
+print("Opening queue: ", od.ObjectName)
 
 q = mq.Queue(qmgr, od, CMQC.MQOO_BROWSE)
 
@@ -47,6 +83,7 @@ while ok:
         headers = True
         offset = 0
         ccsid = md['CodedCharSetId']
+        encoding = md['Encoding']
 
         # Iterate through headers that we might expect to see until there are no more.
         while headers:
@@ -66,6 +103,7 @@ while ok:
                 emd = mq.XQH().get_embedded_md(msg)
                 fmt = emd['Format']
                 ccsid = emd['CodedCharSetId']
+                encoding = emd['Encoding']
                 # print(f'G = {emd['GroupId']}')
 
                 print()
@@ -78,13 +116,15 @@ while ok:
                 mde = mq.MDE().unpack(msg[offset:offset + CMQC.MQMDE_CURRENT_LENGTH])
                 fmt = mde['Format']
                 ccsid = mde['CodedCharSetId']
+                encoding = mde['Encoding']
+
                 offset += CMQC.MQMDE_CURRENT_LENGTH
 
                 print(mde.to_string())
 
             elif fmt == CMQC.MQFMT_RF_HEADER_2:
                 rfh2 = mq.RFH2()
-                rfh2.unpack(msg[offset:], md.Encoding)
+                rfh2.unpack(msg[offset:], encoding)
                 fmt = rfh2['Format']
                 offset += rfh2['StrucLength']
 
@@ -99,9 +139,55 @@ while ok:
                 dlh.unpack(msg[offset:offset + CMQC.MQDLH_CURRENT_LENGTH])
                 fmt = dlh['Format']
                 ccsid = dlh['CodedCharSetId']
+                encoding = dlh['Encoding']
+
                 offset += CMQC.MQDLH_CURRENT_LENGTH
 
                 print(dlh.to_string())
+
+            elif fmt == CMQC.MQFMT_ADMIN or fmt == CMQC.MQFMT_EVENT:
+                # This is not suitable for handling PCF commands and their responses. But it does work for reading other
+                # PCF-formatted messages such as events. There are no message body contents or additional headers after
+                # events, so set the offset to the end.
+
+                # The unpack call returns everything already decoded. So we don't need to work out if the field is
+                # an int or string. This method returns a tuple with the MQCFH as the second element
+                evt = mq.PCFExecute.unpack(msg)
+
+                parms=evt[0]
+                cfh = evt[1]
+
+                print(f"Command: {mq.CMQSTRC.MQCMD_DICT.get(cfh.Command)}")
+
+                # Print out the contents. There's no attempt to make this look pretty, unlike amqsevt. But it shows how
+                # to walk through the elements.
+                keys = parms.keys()
+                for k in keys:
+                    # An event message may contain groups. But there's no additional nesting.
+                    # So we can walk through the group contents without needing to worry about recursion.
+                    if mq.CMQC.MQGA_FIRST <= k <= mq.CMQC.MQGA_LAST:
+                        group = parms[k]
+                        for i in range(0,len(group)):
+                            gkeys = group[i].keys()
+
+                            print(f"  Group: {mq.CMQSTRC.MQGACF_DICT.get(k)}")
+
+                            for gk in gkeys:
+                                s = lookup(gk)
+                                v = group[i][gk]
+                                if s.startswith("MQCA"):
+                                    v = mq.to_string(v)
+                                print(f"    {s:<32s} : {v}")
+
+                    else:
+                        s = lookup(k)
+                        v = parms[k]
+                        if s.startswith("MQCA"):
+                            v = mq.to_string(v)
+                        print(f"  {s:<32s} : {v}")
+
+                offset += len(msg)
+                headers = False
 
             else:
                 headers = False
